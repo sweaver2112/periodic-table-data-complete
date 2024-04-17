82db510795116076cf09857abd5d2f8647e9900d
Scott Weaver <sweaver2112@gmail.com>
2024-04-16 17:44:29 -0700

    updated test file to use the obj

--- src/test.js
+++ src/test.js
@@ -1,5 +1,10 @@
 import {pTable, pTableUnits, pTableProperties} from '../index.js';
 
+var pt = JSON.parse(pTable)
+var units = JSON.parse(pTableUnits)
+var props = JSON.parse(pTableProperties)
+console.log(`>Props (abundance):${props.abundance}\n>Second element universal abundance:${pt[1].abundance.universe}${units.abundance.universe } }`)
+
 console.log("----------Table----------");
 console.log(pTable);
 console.log("----------Props----------");
